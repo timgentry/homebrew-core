f6057901b50dd7fe87ad61922378f2f8470de04e
Tim Gentry <52189+timgentry@users.noreply.github.com>
2022-06-20 17:01:03 +0100

    Create necto-studio.rb

---
+++ Casks/necto-studio.rb
@@ -0,0 +1,11 @@
+cask "necto-studio" do
+  version "2.0.0"
+  sha256 "d9cf405f6965cda13ddd68229219e1a1022aa71b864090eec79845c97742e73b"
+
+  url "https://download.mikroe.com/documents/necto/NectoStudio-x64-online.dmg"
+  name "NECTO Studio"
+  desc "NECTO Studio microcontroller IDE"
+  homepage "https://www.mikroe.com/necto"
+
+  app "NECTOStudio.app"
+end
